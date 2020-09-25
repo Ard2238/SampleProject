914ac09881aad3e90226b79b26dd3add56aa719d
Ard2238 <abhishekrdas28@gmail.com>
2020-09-25 09:21:34 +0530

    UC1 completed. Added Employee Presence Program

---
+++ Sample/src/EMWCUC1.java
@@ -0,0 +1,16 @@
+public class Sample{
+
+	public static void main(String[] args){
+
+		int present =1;
+		int checkattendance =(int) (Math.random() * 10) % 2;
+	
+		if(checkattendane == present){
+			System.out.println("Employee is present");
+		}else{
+			System.out.println("Employee is absent");
+		}
+	}
+}	
+
+}
